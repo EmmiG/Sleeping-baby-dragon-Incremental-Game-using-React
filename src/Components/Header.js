f723cd58e992945aeb5c8aa0872c474cc2659602
Dragoness390 <cera11@hotmail.se>
2018-12-16 12:10:12 +0100

    Fixing header by adding divs with classnames, paragraph and description text for the game

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 /*login*/
 
-function Header(){
+function Header(props){
   return(
     <header>
-      <h1 id="logo-title">Sleeping baby dragon</h1>
+      <div className="header-login-container">
+      <h1 id="logo-title-header">Sleeping baby dragon</h1>
+      <div className="header-description">
+      <p> <b>Description of the game:</b>
+      Sleeping Dragon Increment clicking game is build with react.js. Version 1 will be a image of a dragon where you can click to be able to unlock 5 different upgrades. In the future you will be able to get different costumes and much more to make the clicking more fun. </p> 
+      
+       </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
