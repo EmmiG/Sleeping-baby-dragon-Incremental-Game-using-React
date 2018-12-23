dcc6cd0442c008af6413c09af31f2013be5e8101
Dragoness390 <cera11@hotmail.se>
2018-12-23 16:57:06 +0100

    cleaning the files that arent used in the project, and InputField.js were one of them

--- src/Components/InputField.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-/* login */
-
-function InputField(props){
-  return (<input type="text" onChange={props.handleChange} value={props.searchTerm} />);
-}
-
-export default InputField;
