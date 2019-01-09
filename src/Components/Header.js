54cb23b2d6d810a9157d838893d7f43ac5784c3c
Dragoness390 <cera11@hotmail.se>
2019-01-09 18:29:24 +0100

    Fixed: starting styling the mobile version I always start from the top and made the header login text size fitable for the mobile version, made some space between in the top of header login. Created space between the Dragonimage. After that I went over to the Header.js and changed the divs to create a header 2 which will be a title for the description of the game. I made a almost full width on the header login container. I styled text size on the titles and text size of the description itself. Made all the description text in the middle.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,9 +7,14 @@ function Header(props){
     <header>
       <div className="header-login-container">
       <h1 id="logo-title-header">Sleeping baby dragon</h1>
+      <div className="header-title">
+      <h2 id="header-description-title">Description of the game:</h2>
+      </div>
       <div className="header-description">
-      <p> <b>Description of the game:</b>
-      Sleeping Dragon Increment clicking game is build with react.js. Version 1 will be a image of a dragon where you can click to be able to unlock 5 different upgrades. In the future you will be able to get different costumes and much more to make the clicking more fun. </p> 
+      <p id="description-game"> 
+      Sleeping Dragon Increment clicking game is build with react.js. 
+      Version 1 will be an image of a dragon where you can click to be able to unlock 5 different upgrades. 
+      In the future you will be able to get different costumes and much more to make the clicking more fun. </p> 
       
        </div>
       </div>
