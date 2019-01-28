a6484507e71aabdf1eb899c9f92569030ad3d4eb
Dragoness390 <cera11@hotmail.se>
2019-01-28 17:18:42 +0100

    Fixed: DragonUpgradesContainer.js added comments

--- src/Components/DragonUpgradesContainer.js
+++ src/Components/DragonUpgradesContainer.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import DragonUpgrades from './DragonUpgrades.js';
-//import Footer from './Footer.js';
+
+/*
+    * Goal for the DragonUpgradesContainer Component: container that displayed a basket
+    * with different upgrades which will rise in price when bought.
+    * Once any upgrade is bought it will automaticly make the snore go up every
+    * 2 second so you dont need to click as much. 
+*/
+
+/*
+    * made a list of upgrades which is avialable once the other upgrades in the
+    * ShopUpgrades.js component which is shown in the UserStatsContainer.js 
+    * component is sold out. As before I created the name of the upgrade, itemCost
+    * but also added an image its blank for now because I dont have an image but
+    * it will be in the images folder later.
+*/
 
 const basketInventory = [
   {
@@ -35,6 +49,14 @@ const basketInventory = [
   },
 ];
 
+/*
+    * Made a class of DragonUpgradeContainer which will map all the
+    * slot rows as and array and return it with index. I get the 
+    * DragonUpgrades.js component and you have to set the key and the index
+    * itself so the index will get updated when one buys a basket item.
+    * Added the props of buyDragonUpgrade and totalSnore.
+*/
+
 class DragonUpgradesContainer extends Component {
   render() {
     let slots = [];
@@ -49,6 +71,12 @@ class DragonUpgradesContainer extends Component {
         />
       );
     });
+
+/*
+    * After that I return a div className called upgrade-container with 
+    * a header called basket-header-title which is a h1 then another div
+    * that contain the row slots and it will display the list here.
+*/
     return (
       <div className="upgrade-container col-xs-12 col-md-6 order-md-1">
         <h1 className="basket-header-title">Basket</h1>
